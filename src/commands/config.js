4d67b41920d752dcc98c576ba2865955cd2f0cb1
MatterMr <48224224+MatterMr@users.noreply.github.com>
2023-07-03 14:59:18 -0400

    Add the basic format for the config and sync commands

---
+++ src/commands/config.js
@@ -0,0 +1,4 @@
+exports.command = 'config'
+exports.describe = 'Set tool functionality and automation'
+exports.builder = {}
+exports.handler = async function (argv) {}
---
+++ src/commands/sync.js
@@ -0,0 +1,4 @@
+exports.command = 'sync'
+exports.describe = 'Sync the Canvas Module and Notion Workspace'
+exports.builder = {}
+exports.handler = async function (argv) {}
