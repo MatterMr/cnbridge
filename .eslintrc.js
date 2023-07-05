dcd636ccd2876de7dba7fd56632157a33a81c684
MatterMr <48224224+MatterMr@users.noreply.github.com>
2023-07-04 21:42:14 -0400

    added prettier-eslint support

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,22 @@ module.exports = {
   env: {
     commonjs: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: 'standard',
+  extends: ["standard", "prettier"],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: "script",
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest",
   },
-  rules: {
-  }
-}
+  rules: {},
+};
