c9409b9b7d332445bd53bda3d8b99f01e0c46d09
MatterMr <48224224+MatterMr@users.noreply.github.com>
2023-07-11 15:05:56 -0600

    Added getAllCourses

--- src/services/CanvasLoader.js
+++ src/services/CanvasLoader.js
@@ -1,5 +1,33 @@
+const { GraphQLClient, gql } = require("graphql-request");
+
 module.exports = class CanvasLoader {
-  constructor(API_TOKEN) {
-    this._API_TOKEN = API_TOKEN;
+  constructor(ENDPOINT, CANVAS_API_TOKEN) {
+    this._API_TOKEN = CANVAS_API_TOKEN;
+    this._graphQLClient = new GraphQLClient(ENDPOINT + "/api/graphql", {
+      headers: {
+        authorization: `Bearer ${CANVAS_API_TOKEN}`,
+      },
+    });
+  }
+  /**
+   * Gets all the Courses accesable to the user
+   *
+   * @typedef {Object} allCourses
+   * @property {string} allCourses[].name - The name of the course
+   * @property {string} allCourses[].id - The id of the course
+   *
+   * @return {allCourses}
+   */
+  async getAllCourses() {
+    const query = gql`
+      query MyQuery {
+        allCourses {
+          name
+          id
+        }
+      }
+    `;
+    const data = await this._graphQLClient.request(query);
+    return await data.allCourses;
   }
 };
