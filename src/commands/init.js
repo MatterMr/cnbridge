5022bf8ac38ab1088c9ebdc577a35dcb5c084d5f
MatterMr <48224224+MatterMr@users.noreply.github.com>
2023-07-11 15:06:23 -0600

    package upgrade

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,4 +1,5 @@
-exports.command = 'init'
-exports.describe = 'The initial setup and sync of the Canvas Module and Notion Workspace'
-exports.builder = {}
-exports.handler = async function (argv) {}
+exports.command = "init";
+exports.describe =
+  "The initial setup and sync of the Canvas Module and Notion Workspace";
+exports.builder = {};
+exports.handler = async function (argv) {};
