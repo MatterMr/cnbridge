55e4b50ea32e79c5bf3cf1c8cabe1f3c16ab8809
MatterMr <48224224+MatterMr@users.noreply.github.com>
2023-07-13 16:44:44 -0600

    Centralized the GQLRequest with some Error Formatting

--- src/services/CanvasLoader.js
+++ src/services/CanvasLoader.js
@@ -4,10 +4,39 @@ module.exports = class CanvasLoader {
   constructor(ENDPOINT, CANVAS_API_TOKEN) {
     this._API_TOKEN = CANVAS_API_TOKEN;
     this._graphQLClient = new GraphQLClient(ENDPOINT + "/api/graphql", {
+      errorPolicy: "all",
       headers: {
         authorization: `Bearer ${CANVAS_API_TOKEN}`,
       },
     });
+  }
+  /**
+   * Makes a GraphQL requests
+   *
+   * @param {String} GraphQL - GraphQL query string
+   * @return {Object} Response
+   */
+  async _GQLRequest(query) {
+    const formattedQuery = gql`
+      ${query}
+    `;
+    return await this._graphQLClient
+      .request(formattedQuery)
+      .catch((err) => this._GQLErrorHandler(err.response));
+  }
+  /**
+   * Handles the formatting of errors
+   *
+   * ! In the future this function should handle cases where data is returned and the rest can be logged
+   *
+   * @param {Array} errors - Array of errors
+   */
+  _GQLErrorHandler(response) {
+    const errorBatch = [];
+    response.errors.forEach((err) => {
+      errorBatch.push(new Error(`${err.extensions.code} : ${err.message}`));
+    });
+    throw errorBatch;
   }
   /**
    * Gets all the Courses accesable to the user
@@ -19,15 +48,17 @@ module.exports = class CanvasLoader {
    * @return {allCourses}
    */
   async getAllCourses() {
-    const query = gql`
+    const query = `
       query MyQuery {
         allCourses {
           name
           id
+          nood
+          nd
         }
       }
     `;
-    const data = await this._graphQLClient.request(query);
-    return await data.allCourses;
+    const response = await this._GQLRequest(query);
+    return response.allCourses;
   }
 };
